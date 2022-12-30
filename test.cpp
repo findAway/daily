c875dc5e0903ef8ad5f0a1318127d783154023f9
XiaoJihuaQiniu <xiaojihua@qiniu.com>
2022-12-30 21:53:14 +0800

    更新test.cpp添加新函数test2

--- test.cpp
+++ test.cpp
@@ -3,5 +3,12 @@
 int test1()
 {
     std::cout << "Hi, everyone, this is test1" << std::endl;
+    std::cout << "Hi, add function in branch1" << std::endl;
+    return 0;
+}
+
+int test2()
+{
+    std::cout << "Hi, everyone, this is test2" << std::endl;
     return 0;
 }
